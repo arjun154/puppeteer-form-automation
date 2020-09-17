4632e19e04e84c9fb3b59cc2a58d55bd935e20e0
Sandeep Babu <sandeep@revvsales.com>
2020-09-17 10:03:03 +0530

    chore: added comments

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,33 @@ const express = require('express');
 const app = express();
 
 app.get('/screenshot', async (req, res) => {
+  //opens the browser
   const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
+
+  //loads the url
   await page.goto('https://auth.revvsales.com/signin');
+
+  //authentication
   await page.type('#signin-email-field', 'sandeep.bsn@gmail.com');
   await page.click('#signin-email-continue-btn');
   await page.waitForTimeout(2000)
   await page.type('#signin-password-field', 'Sandyhappy_9596');
+
+  //navigating to landing page
   await Promise.all([
     page.waitForNavigation(), // The promise resolves after navigation has finished
     page.click('#signin-button-field'), // Clicking the link will indirectly cause a navigation
   ]);
 
+  //navigating to the document editing page
   await Promise.all([
     page.waitForNavigation(), // The promise resolves after navigation has finished
     page.goto('https://ft392m2a.revvsales.com/documents/DOC-000020'), // Clicking the link will indirectly cause a navigation
   ]);
   await page.waitForTimeout(2000);   
   
+  //entering values in the form fields 
   await page.focus('.revv-inp')
   await page.keyboard.down('Control');
   await page.keyboard.press('A');
@@ -38,7 +47,11 @@ app.get('/screenshot', async (req, res) => {
   await page.keyboard.type('2500', {delay: 100})
 
   await page.waitForTimeout(2000); 
+
+  //taking screenshot of the page
   await page.screenshot({path: 'login.png'});
+
+  //close the browser. now with the help of doc_no, doc_id and object_id, you can create a magic link of the document using revvsales api's
   await browser.close();
   res.json({"name":"babu"})
 })
